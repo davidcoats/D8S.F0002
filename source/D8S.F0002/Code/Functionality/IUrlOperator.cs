175248e717f819b9d1b14472b72bcd79fd258214
davidcoats <david@getsafetycone.com>
2023-04-24 00:22:00 -0700

    Interim changes.

--- source/D8S.F0002/Code/Functionality/IUrlOperator.cs
+++ source/D8S.F0002/Code/Functionality/IUrlOperator.cs
@@ -8,13 +8,64 @@ namespace D8S.F0002
     [FunctionalityMarker]
     public partial interface IUrlOperator : IFunctionalityMarker
     {
-        public string GetArticleImageUrl(
+        /// <summary>
+        /// When uploading an image, puts it at: {S3 Bucket Name}/Blog-Personal/{Article Name Url Segment}/Images/{imageFileNameUrlSegment}.
+        /// <para>
+        /// This means creating both the {Article Name Url Segment} directory in the bucket, and the Images directory in the article name URL segment directory.
+        /// </para>
+        /// </summary>
+        public string GetArticleImageUrl_Blog_Technical(
             string articleNameUrlSegment,
             string imageFileNameUrlSegment)
         {
             var publicBucketUrl = Instances.Urls.S3PublicBucket;
 
             var articleImageUrl = $"{publicBucketUrl}/Blog-Technical/Articles/{articleNameUrlSegment}/Images/{imageFileNameUrlSegment}";
+            return articleImageUrl;
+        }
+
+        /// <summary>
+        /// <para>Chooses <see cref="GetArticleImageUrl_Blog_Technical(string, string)"/> as the default.</para>
+        /// <inheritdoc cref="GetArticleImageUrl_Blog_Technical(string, string)" path="/summary"/>
+        /// </summary>
+        public string GetArticleImageUrl(
+            string articleNameUrlSegment,
+            string imageFileNameUrlSegment)
+        {
+            return this.GetArticleImageUrl_Blog_Technical(
+                articleNameUrlSegment,
+                imageFileNameUrlSegment);
+        }
+
+        /// <summary>
+        /// When uploading an image, puts it at: {S3 Bucket Name}/Blog-Personal/{Article Name Url Segment}/Images/{imageFileNameUrlSegment}.
+        /// <para>
+        /// This means creating both the {Article Name Url Segment} directory in the bucket, and the Images directory in the article name URL segment directory.
+        /// </para>
+        /// </summary>
+        public string GetArticleImageUrl_Blog_Personal(
+            string articleNameUrlSegment,
+            string imageFileNameUrlSegment)
+        {
+            var publicBucketUrl = Instances.Urls.S3PublicBucket;
+
+            var articleImageUrl = $"{publicBucketUrl}/Blog-Personal/Articles/{articleNameUrlSegment}/Images/{imageFileNameUrlSegment}";
+            return articleImageUrl;
+        }
+
+        /// <summary>
+        /// When uploading an image, puts it at: {S3 Bucket Name}/Blog-TheLegFiles/{Article Name Url Segment}/Images/{imageFileNameUrlSegment}.
+        /// <para>
+        /// This means creating both the {Article Name Url Segment} directory in the bucket, and the Images directory in the article name URL segment directory.
+        /// </para>
+        /// </summary>
+        public string GetArticleImageUrl_Blog_TheLegFiles(
+            string articleNameUrlSegment,
+            string imageFileNameUrlSegment)
+        {
+            var publicBucketUrl = Instances.Urls.S3PublicBucket;
+
+            var articleImageUrl = $"{publicBucketUrl}/Blog-TheLegFiles/Articles/{articleNameUrlSegment}/Images/{imageFileNameUrlSegment}";
             return articleImageUrl;
         }
     }
--- source/D8S.F0002/Code/Values/IUrls.cs
+++ source/D8S.F0002/Code/Values/IUrls.cs
@@ -8,6 +8,9 @@ namespace D8S.F0002
     [ValuesMarker]
     public partial interface IUrls : IValuesMarker
     {
+        /// <summary>
+        /// public.bucket.8acc798b-9220-4d85-9a78-551f46ec1dd2
+        /// </summary>
         public string S3PublicBucket => "https://s3.us-west-1.amazonaws.com/public.bucket.8acc798b-9220-4d85-9a78-551f46ec1dd2";
     }
 }
